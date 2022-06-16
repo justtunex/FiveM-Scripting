aca7f680eb74dc937e291ecc5d0f313617c7677a
justtunex <80111311+justtunex@users.noreply.github.com>
2022-06-16 22:19:17 +0200

    Update index.html

---
+++ idk/client/main.lua
@@ -0,0 +1,19 @@
+-- idk what script to do so i just look whats comming out 
+
+
+-- 1 idee GTA Online Heist 
+
+
+local hasHeistStarted = false 
+local playerCount = 0 
+
+
+function joinHeistMenu()
+    local options = {
+        
+    }
+
+    ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'join_menu', {
+
+    })
+end
