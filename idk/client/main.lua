d19a5f10b535ef246e02207f944f2ece46159c2b
justtunex <80111311+justtunex@users.noreply.github.com>
2023-09-30 20:21:22 +0000

    Pending changes exported from your codespace

--- idk/client/main.lua
+++ idk/client/main.lua
@@ -5,15 +5,25 @@
 
 
 local hasHeistStarted = false 
-local playerCount = 0 
+local lobbys = 0
+local lobbyCount = 0 
 
 
 function joinHeistMenu()
     local options = {
-        
+        {label = "Lobby Erstellen", value = "create"}, 
+        {label = "Lobby Beitreten", valiue = "join"}
     }
 
     ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'join_menu', {
-
-    })
+        title = "Heist Menu", 
+        align = "left",
+        elements = options
+    }, function(data, menu)
+        if (data.current.value == "create") then 
+            
+        end
+    end, function(data, menu)
+        menu.close()
+    end)
 end
